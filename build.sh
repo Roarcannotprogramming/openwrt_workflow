a51093b2cce9745f06dffffc0771dfc3eb266167
V1me <z1991998920@gmail.com>
2022-05-01 18:52:18 +0800

    sss

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
+sudo apt-get update
+sudo apt-get install upx -y
+ln -s /usr/bin/upx staging_dir/host/bin/upx
+ln -s /usr/bin/upx-ucl staging_dir/host/bin/upx-ucl
+
+./scripts/feeds update -a
+
+cp -r /home/build/openwrt_workflow/openwrt-passwall-3aff3af88536227d12fb7206992af64ff21cf4d2/luci-app-passwall/ /home/build/openwrt/feeds/passwall/
+./scripts/feeds update -a
+./scripts/feeds install luci-app-passwall
+make defconfig
+make package/luci-app-passwall/compile V=s -j1
+
+pushd bin/packages/x86_64
+tar zcvf passwall.tar.gz passwall/
+popd
